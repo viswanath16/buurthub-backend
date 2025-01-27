cc305e34f3d7d0968260ea32e28421e13c489243
Viswanath Kambhampati <viswanath.chaitanya.kumar.kambhampati@ing.com>
2025-01-27 11:01:45 +0100

    Initial commit

---
+++ buurthub-services/src/main/java/com/backend/buurthub/BuurtHubApplication.java
@@ -0,0 +1,13 @@
+package com.backend.buurthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuurtHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuurtHubApplication.class, args);
+	}
+
+}
---
+++ buurthub-services/src/test/java/com/backend/buurthub/BuurtHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.buurthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuurtHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
